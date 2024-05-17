ca82fddfdf96aa000c9380acd7c44ffcddf4f3a6
kxzb.fun <sunday_ever@163.com>
2024-05-17 14:10:59 +0800

    feat: server both in development and production mode can use

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Now the frontend is also fine, working with the server both in development and production mode.
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    }
+  },
 })
